e09cb911a3e24febcaffc4c8e9d74511a8f85275
suli ajibola <suli.ajibola.dev@gmail.com>
2023-02-15 06:26:27 -0500

    another Minor(reactions updated)

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -2,12 +2,17 @@ import React from "react";
 import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
 import { selectAllPosts } from "./postSlice";
+import ReactionButtons from "./ReactionButtons";
 import TimeAgo from "./TimeAgo";
 
 const PostList = () => {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPost = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPost.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)} </p>
@@ -15,6 +20,7 @@ const PostList = () => {
         <PostAuthor userId={post.userId} />
         <TimeAgo timeStamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ));
   return (
