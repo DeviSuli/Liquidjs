0ca473406a70ffe8c7384b0ace80b97924321f4a
suli ajibola <suli.ajibola.dev@gmail.com>
2023-02-15 04:34:26 -0500

    part commit

---
+++ src/features/post/postSlice.js
@@ -0,0 +1,46 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+
+const initialState = [
+  {
+    id: "1",
+    title: "learning Redux Toolkit",
+    content: "I've heard good things",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  },
+];
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+          },
+        };
+      },
+    },
+  },
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postSlice.actions;
+
+export default postSlice.reducer;
